73d5a034a72cd16cb51d63094bae23a45732ccc3
Krit-Jain <kritj2005@gmail.com>
2026-01-03 11:36:47 +0530

    train.py removed

--- src/train.py
+++
@@ -1,7 +0,0 @@
-"""
-Entry point for training tabular and multimodal models.
-For full experiments, see notebooks/model_training.ipynb
-"""
-
-print("Training is notebook-driven.")
-print("See notebooks/model_training.ipynb")
